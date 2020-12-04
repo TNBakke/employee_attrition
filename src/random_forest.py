6f339eee7d46df857acd3d0efd9329ed9d8f5c2e
Tom Bakke <tom.bakke@gmail.com>
2020-12-03 17:51:17 -0700

    cleaned up directories

--- src/random_forest.py
+++ src/random_forest.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler 
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split, cross_val_score
 
 def default_random_forest(df):
+    """Generate a defual Random Forest for DataFrame
+
+    Args:
+        df ([DataFrame]): Attrition DataFrame 
+
+    Returns:
+        [Tuple]: Tuple with accuracy, precision and recall scores
+    """    
     y = df.pop('attrition').values
     X = df.values
     X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=42)
@@ -22,46 +31,31 @@ def default_random_forest(df):
     return f'Accuracy Score: {accuracy_score_}', f'Precision Score: {precision_score_}', f'Recall Score: {recall_score_}'
 
 
-def smote_default_random_forest(df):
-    y = df.pop('attrition').values
-    X = df.values
-    X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=42)
-    smt = SMOTE(random_state=42)
-    X_train_SMOTE, y_train_SMOTE = smt.fit_sample(X_train, y_train)
-    rf = RandomForestClassifier(random_state=42)
-    rf.fit(X_train_SMOTE, y_train_SMOTE)
-    y_preds = rf.predict(X_test)
+def random_forest_grid(df):
+    """Using CV to find the most optimal hyperparameters for random forest model
+
+    Args:
+        df ([DataFrame]): Attrition DataFrame
+
+    Returns:
+        [list]: List of best hyperparameters
+    """   
+    rf = RandomForestClassifier(random_state=42, class_weight = 'balanced')
+    rf_random = RandomizedSearchCV(estimator=rf, param_distributions = grid_param, n_iter = 500,
+                                cv=5, verbose=2, random_state=42, n_jobs=-1)
+    y = df.attrition
+    X = df.drop('attrition',axis=1)
     
-    accuracy_score_ = accuracy_score(y_test, y_preds)
-    precision_score_ = precision_score(y_test, y_preds)
-    recall_score_ = recall_score(y_test, y_preds)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+    scaler = StandardScaler()
+    scaler.fit(X_train)
     
-    return f'Accuracy Score: {accuracy_score_}', f'Precision Score: {precision_score_}', f'Recall Score: {recall_score_}'
-
-
-def grid_search_wrapper(refit_score='precision_score'):
-    """
-    fits a GridSearchCV classifier using refit_score for optimization
-    prints classifier performance metrics
-    """
-    skf = StratifiedKFold(n_splits=10)
-    grid_search = GridSearchCV(rf, param_grid, scoring=scorers, refit=refit_score,
-                           cv=skf, return_train_score=True, n_jobs=-1)
-    grid_search.fit(X_train.values, y_train.values)
-
-    # make the predictions
-    y_pred = grid_search.predict(X_test.values)
-
-    print('Best params for {}'.format(refit_score))
-    print(grid_search.best_params_)
-
-    # confusion matrix on the test data.
-    print('\nConfusion matrix of Random Forest optimized for {} on the test data:'.format(refit_score))
-    print(pd.DataFrame(confusion_matrix(y_test, y_pred),
-                 columns=['pred_neg', 'pred_pos'], index=['neg', 'pos']))
-    return grid_search
+    rf_random.fit(X_train,y_train)
+    
+    return rf_random.best_params_
 
 
 if __name__ == '__main__':
     # default_random_forest(df)
-    
+    # random_forest_grid(df)
